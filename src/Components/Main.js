5e4b380e8d6b36c32e4fb45f774c2b39dcb771c5
Wollivan <tim.smith2814@gmail.com>
2022-07-06 09:40:38 +0100

    Add Images to component

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,17 +1,23 @@
 import { Component } from "react";
 import HornedBeast from "./HornedBeast";
 
-
 class Main extends Component {
-    render () {
-        return (
-
-        <div className="main">
-            <HornedBeast title="Narwhal" />
-            <HornedBeast title="Unicorn"/>
-        </div>
-        )
-    }
+  render() {
+    return (
+      <div className="main">
+        <HornedBeast
+          title="Narwhal"
+          imageUrl="https://i.natgeofe.com/k/57ae9fdd-2aae-45b8-9e14-af7299fdebcb/narwhal_alone-crop_4x3.jpg"
+          description="The Narwhal is actually a real creature, would you believe it?!"
+        />
+        <HornedBeast
+          title="Unicorn"
+          imageUrl="https://www.sciencenewsforstudents.org/wp-content/uploads/2020/03/1030_onwardunicorns-1-1028x579.png"
+          description="I also believe that Unicorns are real, we just haven't found any yet..."
+        />
+      </div>
+    );
+  }
 }
 
 export default Main;
